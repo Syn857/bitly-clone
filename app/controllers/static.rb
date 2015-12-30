ea77745b3839bee5f718718160cb71816a5c7089
Josiah Peter James <josiahpeter29@gmail.com>
2015-12-30 15:00:42 +0800

    almost done with clone

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,3 +1,17 @@
 get '/' do
+	@urls = Url.all
   erb :"static/index"
+end
+
+post '/urls' do
+	object = Url.new(long_url: params[:long_url])
+	shorten_url = object.shorten
+	object.short_url = shorten_url
+	object.save
+	redirect '/'
+end
+
+
+get '/profile' do
+	erb :"static/profile"
 end
